bf5ec1fb4c55aa684a707c978b7de2b203532a51
Bernard <bernard.babicz@hotmail.com>
2018-07-22 21:57:34 +0200

    Xml_3

--- test/src/main/java/com/caveofprogramming/spring/test/test/AppT.java
+++ test/src/main/java/com/caveofprogramming/spring/test/test/AppT.java
@@ -8,20 +8,7 @@ public class AppT {
 	public static void main(String[] args) {
 		ApplicationContext context= new ClassPathXmlApplicationContext("com/caveofprogramming/spring/test/test/beans/beans.xml");
 		
-//		Person person=(Person)context.getBean("person");
-//		person.speak();
-//		
-//		Address address=(Address)context.getBean("address");
-//		System.out.println(address);
-		
-		//Person person=(Person)context.getBean("person");
-		//Person person2=(Person)context.getBean("person");
-		
-		//person.setTaxId(666);
-		
-		//System.out.println(person);
-		
-		//Address address2=(Address)context.getBean("address2");
+
 		FruitBasket basket=(FruitBasket)context.getBean("basket");
 		System.out.println(basket);
 		
--- test/src/main/java/com/caveofprogramming/spring/test/test/Jungle.java
+++ test/src/main/java/com/caveofprogramming/spring/test/test/Jungle.java
@@ -5,40 +5,28 @@ import java.util.List;
 import java.util.Map;
 
 public class Jungle {
-	private Animal largest;
-	private List<Animal> animals;
-	
+
+
 	private Map<String, String> foods=new HashMap<String, String>();
-	
+	private Map<String, Animal> animals=new HashMap<String, Animal>();
 	
 	public void setFoods(Map<String, String> foods) {
 		this.foods = foods;
 	}
-	public Animal getLargest() {
-		return largest;
-	}
-	public void setLargest(Animal largest) {
-		this.largest = largest;
-	}
-	public List<Animal> getAnimals() {
-		return animals;
-	}
-	public void setAnimals(List<Animal> animals) {
+
+	
+	public void setAnimals(Map<String, Animal> animals) {
 		this.animals = animals;
 	}
 	@Override
 	public String toString() {
 		
 		StringBuilder sb=new StringBuilder();
-		sb.append("Najwieksze zwierze:"+largest);
-		sb.append("\n");
-		sb.append("Pozostałe zwierzeta:\n");
-		
-		for (Animal animal : animals) {
-			sb.append(animal);
-			sb.append("\n");
-		}
+
 		for (Map.Entry<String, String> en : foods.entrySet()) {
+			sb.append(en.getKey()+": "+ en.getValue()+"\n");
+		}
+		for (Map.Entry<String, Animal> en : animals.entrySet()) {
 			sb.append(en.getKey()+": "+ en.getValue()+"\n");
 		}
 		
